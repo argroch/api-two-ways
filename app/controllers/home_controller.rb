3c382cc5d484742c99c766d41df9e8a0f34e5dc9
Aaron Groch <ar.groch@gmail.com>
2015-11-30 21:52:09 -0500

    using nokogiri to pull data from xml ftp

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+
+  require 'nokogiri'
+  require 'open-uri'
+
+  def index
+    results = HTTParty.get("http://techtalentstore.herokuapp.com/products.json")
+    @product1 = results[""]["namespace"]
+  end
+
+  def xml_parse
+    @doc = Nokogiri::HTML(open("ftp://ftp.fec.gov/FEC/data.fec.gov/candidate_disbursement2016/GA_05.xml"))
+
+    @description = @doc.at('// description').text
+
+    @expense_list = []
+
+    @doc.xpath('//rec_nam').each_with_index do |x,index|
+      expense = []
+      expense.push(x.text)
+      expense.push(@doc.xpath('//dis_amo')[index].text)
+      @expense_list.push(expense)
+    end
+  end
+end
